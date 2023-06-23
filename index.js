cee618423feec8ff9b8c5eae5d3fbc534083fd35
acristianf <acristianf@hotmail.com>
2023-06-23 20:15:20 -0300

    basic functionality complete

--- index.js
+++ index.js
@@ -1,22 +1,102 @@
-require('dotenv').config();
+require("dotenv").config()
 const express = require('express');
 const cors = require('cors');
+const qs = require("querystring")
+const dns = require("dns");
+const mongoose = require("mongoose");
 const app = express();
 
-// Basic Configuration
 const port = process.env.PORT || 3000;
 
 app.use(cors());
 
 app.use('/public', express.static(`${process.cwd()}/public`));
 
-app.get('/', function(req, res) {
+const start = async () => {
+  try {
+    await mongoose.connect(process.env.MONGO_URI);
+  } catch (e) {
+    console.log("ERROR: " + e);
+    process.exit(1);
+  }
+}
+start();
+
+const urlSchema = mongoose.Schema({
+  original_url: {
+    type: String,
+    required: true
+  },
+  short_url: {
+    type: Number,
+    required: true
+  }
+})
+const urlModel = mongoose.model("url", urlSchema);
+
+app.get('/', function(_, res) {
   res.sendFile(process.cwd() + '/views/index.html');
 });
 
-// Your first API endpoint
-app.get('/api/hello', function(req, res) {
-  res.json({ greeting: 'hello API' });
+app.post("/api/shorturl", (req, res) => {
+  let data = [];
+  req.on("data", (chunk) => {
+    data.push(chunk);
+  }).on("end", () => {
+    data = Buffer.concat(data).toString();
+    var dataObj = qs.parse(data);
+    dns.lookup(dataObj["url"].replace("https://", ""), (err) => {
+      if (err) res.json({ error: "invalid url" });
+      else {
+        urlModel.findOne({ original_url: dataObj["url"] })
+          .select("original_url short_url -_id")
+          .exec()
+          .then((d) => {
+            if (d) {
+              res.json(d);
+            } else {
+              urlModel.findOne()
+                .sort("-short_url")
+                .exec()
+                .then((found) => {
+                  if (!found) {
+                    urlModel.create({
+                      original_url: dataObj["url"],
+                      short_url: 1
+                    });
+                  } else {
+                    let current_short_url = found["short_url"];
+                    current_short_url += 1;
+                    const doc = new urlModel({
+                      original_url: dataObj["url"],
+                      short_url: current_short_url
+                    });
+                    doc.save();
+                    res.json({
+                      original_url: doc["original_url"],
+                      short_url: doc["short_url"]
+                    });
+                  };
+                });
+            };
+          });
+      };
+    });
+  });
+});
+
+app.get("/api/shorturl/:short_url?", (req, res) => {
+  urlModel.findOne({ short_url: req.params.short_url })
+    .select("original_url")
+    .exec()
+    .then((f) => {
+      if (f) res.redirect(f["original_url"]);
+      else {
+        res.json({
+          error: "No short URL found for the given input"
+        })
+      }
+    });
 });
 
 app.listen(port, function() {
