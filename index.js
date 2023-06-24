249650bcc712049fc9d4221aca4d1b14d1763591
acristianf <acristianf@hotmail.com>
2023-06-23 21:09:02 -0300

    used body-parser

--- index.js
+++ index.js
@@ -1,14 +1,18 @@
 require("dotenv").config()
 const express = require('express');
 const cors = require('cors');
-const qs = require("querystring")
 const dns = require("dns");
 const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+
 const app = express();
 
 const port = process.env.PORT || 3000;
 
 app.use(cors());
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
 
 app.use('/public', express.static(`${process.cwd()}/public`));
 
@@ -39,57 +43,51 @@ app.get('/', function(_, res) {
 });
 
 app.post("/api/shorturl", (req, res) => {
-  let data = [];
-  req.on("data", (chunk) => {
-    data.push(chunk);
-  }).on("end", () => {
-    data = Buffer.concat(data).toString();
-    var dataObj = qs.parse(data);
-    dns.lookup(dataObj["url"].replace("https://", ""), (err) => {
-      if (err) res.json({ error: "invalid url" });
-      else {
-        urlModel.findOne({ original_url: dataObj["url"] })
-          .select("original_url short_url -_id")
-          .exec()
-          .then((d) => {
-            if (d) {
-              res.json(d);
-            } else {
-              urlModel.findOne()
-                .sort("-short_url")
-                .exec()
-                .then((found) => {
-                  var doc;
-                  if (!found) {
-                    doc = new urlModel({
-                      original_url: dataObj["url"],
-                      short_url: 1
-                    });
-                    doc.save();
-                  } else {
-                    let current_short_url = found["short_url"];
-                    current_short_url += 1;
-                    doc = new urlModel({
-                      original_url: dataObj["url"],
-                      short_url: current_short_url
-                    });
-                    doc.save();
-                  };
-                  res.json({
-                    original_url: doc["original_url"],
-                    short_url: doc["short_url"]
-                  })
+  var dataObj = req.body;
+  dns.lookup(dataObj["url"].replace("https://", ""), (err) => {
+    if (err) res.json({ error: "invalid url" });
+    else {
+      urlModel.findOne({ original_url: dataObj["url"] })
+        .select("original_url short_url -_id")
+        .exec()
+        .then((d) => {
+          if (d) {
+            res.json(d);
+          } else {
+            urlModel.findOne()
+              .sort("-short_url")
+              .exec()
+              .then((found) => {
+                var doc;
+                if (!found) {
+                  doc = new urlModel({
+                    original_url: dataObj["url"],
+                    short_url: 1
+                  });
+                  doc.save();
+                } else {
+                  let current_short_url = found["short_url"];
+                  current_short_url += 1;
+                  doc = new urlModel({
+                    original_url: dataObj["url"],
+                    short_url: current_short_url
+                  });
+                  doc.save();
+                };
+                res.json({
+                  original_url: doc["original_url"],
+                  short_url: doc["short_url"]
                 })
-                .catch(() => {
-                  res.json({ error: "POST: /api/shorturl, findOne, unexpected error" });
-                });
-            };
-          })
-          .catch(() => {
-            res.json({ error: "POST: /api/shorturl, Unexpected error" });
-          });
-      };
-    });
+              })
+              .catch(() => {
+                res.json({ error: "POST: /api/shorturl, findOne, unexpected error" });
+              });
+          };
+        })
+        .catch(() => {
+          res.json({ error: "POST: /api/shorturl, Unexpected error" });
+        });
+    };
   });
 });
 
